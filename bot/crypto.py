8d3957f4a6e15e6861a952c3d31ed8a117f2fcc7
Calvados <flamaster0807@gmail.com>
2018-03-24 14:10:15 +0200

    improved encrypt functions

--- bot/crypto.py
+++ bot/crypto.py
@@ -1,18 +1,58 @@
+from hashlib import sha256
+
 def caesar(text, key, decode=False):
     final = ""
     for symbol in text:
         if not decode:
-            final += chr((ord(symbol) + key))
+            if (symbol != ' ' or symbol != '!' or symbol != ',' or symbol != '.') and symbol.islower():
+                final += chr((ord(symbol) - 97 + key + 26) % 26 + 97)
+            elif (symbol != ' ' or symbol != '!' or symbol != ',' or symbol != '.') and symbol.isupper():
+                final += chr((ord(symbol) - 65 + key + 26) % 26 + 65)
+            else:
+                final += symbol
         else:
-            final += chr((ord(symbol) - key))
+            if (symbol != ' ' or symbol != '!' or symbol != ',' or symbol != '.') and symbol.islower():
+                final += chr((ord(symbol) - 97 - key + 26) % 26 + 97)
+            elif (symbol != ' ' or symbol != '!' or symbol != ',' or symbol != '.') and symbol.isupper():
+                final += chr(((ord(symbol) - 65 - key + 26) % 26) + 65)
+            else:
+                final += symbol
     return final
 
 def vigenere(text, key, decode=False):
     final = ""
-    key *= len(text) // len(key) + 1
+    new_key = ''
+    i, j = 0, 0
+
+    while i < len(text):
+        while j < len(key):
+            if (i < len(text) and j < len(key) and (text[i] == ' ' or text[i] == ',' or text[i] == '!')):
+                new_key += text[i]
+                i += 1
+            elif (i < len(text) and j < len(key) and (text[i] != ' ' or text[i] != ',' or text[i] != '!')):
+                new_key += key[j]
+                i += 1
+                j += 1
+            else:
+                break
+        j = 0
+
     for index, symbol in enumerate(text):
         if not decode:
-            final += chr((ord(symbol) + ord(key[index]) % 26))
+            if (symbol != ' ' or symbol != '!' or symbol != ',' or symbol != '.') and symbol.islower():
+                final += chr((ord(symbol) - 97 + (ord(new_key[index]) - 97) + 26) % 26 + 97)
+            elif (symbol != ' ' or symbol != '!' or symbol != ',' or symbol != '.') and symbol.isupper():
+                final += chr((ord(symbol) - 65 + (ord(new_key[index]) - 97) + 26) % 26 + 65)
+            else:
+                final += symbol
         else:
-            final += chr((ord(symbol) - ord(key[index]) % 26))
+            if (symbol != ' ' or symbol != '!' or symbol != ',' or symbol != '.') and symbol.islower():
+                final += chr((ord(symbol) - 97 - (ord(new_key[index]) - 97) + 26) % 26 + 97)
+            elif (symbol != ' ' or symbol != '!' or symbol != ',' or symbol != '.') and symbol.isupper():
+                final += chr(((ord(symbol) - 65 - (ord(new_key[index]) - 97) + 26) % 26) + 65)
+            else:
+                final += symbol
     return final
+
+def hash(text, decode=False):
+    pass
