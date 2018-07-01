32e3ba41a5d4f0e05e81f39dfd67ad637088b22f
Calvados <flamaster0807@gmail.com>
2018-07-01 19:37:13 +0300

    created class for correct shift

--- bot/crypto.py
+++ bot/crypto.py
@@ -1,6 +1,49 @@
 "module which contains some crypto functions"
 from hashlib import sha256
 
+class Shift(object):
+    def __init__(self, char):
+        self._char = char
+        self._ascii = [
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+            'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+            'w', 'x', 'y', 'z'
+        ]
+        self._cyrillic = [
+            'а', 'б', 'в', 'г', 'ґ', 'д', 'е', 'є', 'ж', 'з', 'и',
+            'і', 'ї', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с',
+            'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ь', 'ю', 'я'
+        ]
+
+    def shift(self, count):
+        isCyrillic = self.is_cyrillic()
+        isLower = self._char.lower() == self._char
+        if isCyrillic:
+            container = self._cyrillic
+            isInContainer = self._char.lower() in container
+            mod = 33
+        else:
+            container = self._ascii
+            isInContainer = self._char.lower() in container
+            mod = 26
+        if not isInContainer:
+            return self._char
+        elif isLower:
+            ind = container.index(self._char)
+            return container[(ind + count) % mod]
+        else:
+            ind = container.index(self._char.lower())
+            return container[(ind + count) % mod].upper()
+
+    def change_char(self, new_char):
+        self._char = new_char
+
+    def is_cyrillic(self):
+        return ord(self._char) >= 1040
+
+    def is_symbol(self):
+        return self._char.lower() in self._cyrillic or self._char.lower() in self._ascii
+
 def caesar(text, key, decode=False, ascii_=True):
     "encode or decode message using caesar cipher"
     if (ascii_):
